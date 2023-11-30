d90acf42bf691d71e4e321325b987e05e0c741fb
Lauty Galeano <galeano.lauty@outlook.com>
2023-11-30 16:20:10 -0300

    create-epic added - no functionallity yet

--- tpfinal/src/main.jsx
+++ tpfinal/src/main.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import SideMenu from './components/SideMenu/SideMenu'
+import { createBrowserRouter,RouterProvider } from 'react-router-dom'
+import App from './App'
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>hello</div>
+  },
+  {
+    path: "/my-projects",
+    element: <div>my-projects</div>
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <SideMenu />
+    <App/>
   </React.StrictMode>,
 )
+//<RouterProvider router={router} /> --test
