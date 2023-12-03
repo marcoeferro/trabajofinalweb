49acde400c74b713bcc0f99adfe31d94207a6ede
Marco Ferro <marcoeferro2@gmail.com>
2023-12-03 19:14:22 -0300

    SIde Menu Component

--- tpfinal/src/main.jsx
+++ tpfinal/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import SideMenu from './components/SideMenu/SideMenu'
+import PlaceHolder from './components/Placeholder/PlaceHolder'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <SideMenu />
+    <PlaceHolder />
   </React.StrictMode>,
 )
