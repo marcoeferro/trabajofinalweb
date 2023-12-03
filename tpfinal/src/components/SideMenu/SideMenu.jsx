7b71e4f8ff01177ecd6f4f962b40569096ca2446
Marco Ferro <marcoeferro2@gmail.com>
2023-12-03 19:13:54 -0300

    SideMenu Component

--- tpfinal/src/components/SideMenu/SideMenu.jsx
+++ tpfinal/src/components/SideMenu/SideMenu.jsx
@@ -1,21 +1,40 @@
-import "./SideMenu.scss"
-
+import React, { useState } from 'react';
+import './SideMenu.scss';
+import profilePlaceholder from './../../assets/img/profileplaceholder.jpg';
 
 function SideMenu() {
-    return (<div className="background">
-        <div className="MenuHeader">
+    const [menuExpanded, setMenuExpanded] = useState(false);
 
+    const handleMenuToggle = () => {
+        setMenuExpanded(!menuExpanded);
+    };
+
+    return (
+        <div className={`menu-container ${menuExpanded ? 'expanded' : ''}`}>
+            <div className='menu-content'>
+                <div className="menu-header">
+                    <p>SARAZA123</p>
+                </div>
+                <div className="menu-body">
+                    <button type="button" className="btn btn-primary">Home</button>
+                    <button type="button" className="btn btn-primary">My Projects</button>
+                    <button type="button" className="btn btn-primary">Stories</button>
+                </div>
+                <div className="menu-footer">
+                    <img src={profilePlaceholder} alt="profile picture" className='footer-profile-picture' />
+                    <div className="footer-text">
+                        <h5>Louise Joy King</h5>
+                        <h6>louisejoyk@email.com</h6>
+                    </div>
+                </div>
+            </div>
+            <div className='menu-burgermenu' onClick={handleMenuToggle}>
+                <span className="material-symbols-outlined">
+                    menu
+                </span>
+            </div>
         </div>
-        <div className="MenuBody">
-            <button type="button" class="btn btn-primary">Home</button>
-            <button type="button" class="btn btn-primary">My Projects</button>
-            <button type="button" class="btn btn-primary">Stories</button>
-        </div>
-        <div className="MenuFooter">
-            <img src="tpfinal\src\assets\img" alt="" />
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error ducimus harum debitis, magni, quaerat adipisci atque explicabo, tempora soluta cupiditate libero omnis odio. Nobis facere hic optio, nesciunt in ratione.</p>
-        </div>
-    </div>)
+    );
 }
 
-export default SideMenu
+export default SideMenu;
