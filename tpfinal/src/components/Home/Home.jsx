4feca1cd9e0f56a1e9b833fd85170976696af2f2
Marco Ferro <marcoeferro2@gmail.com>
2023-12-13 17:45:48 -0300

    quick change unused imports deleted

--- tpfinal/src/components/Home/Home.jsx
+++ tpfinal/src/components/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './Home.scss';
 import { Modal, Button, Box } from '@mui/material';
 import CreateProject from '../create-project';
