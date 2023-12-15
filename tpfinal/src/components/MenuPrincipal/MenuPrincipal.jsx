20dfad07c4b2c542beddcb8030dc6b3f443fa651
Marco Ferro <marcoeferro2@gmail.com>
2023-12-15 08:08:05 -0300

    fix: menu Component display fixed

--- tpfinal/src/components/MenuPrincipal/MenuPrincipal.jsx
+++ tpfinal/src/components/MenuPrincipal/MenuPrincipal.jsx
@@ -1,50 +1,55 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import MenuIcon from '@mui/icons-material/Menu';
 import SettingsIcon from '@mui/icons-material/Settings';
-import './MenuPrincipal.scss';
 import { Link, useLocation } from 'react-router-dom';
-import { Breadcrumbs, Typography } from '@mui/material';
+import { Breadcrumbs, Typography, Toolbar, AppBar, Box } from '@mui/material';
 
 const MenuPrincipal = ({ handleOpenSideMenu }) => {
     const [openSettings, setOpenSettings] = useState(false)
     const location = useLocation();
     const pathnames = location.pathname.split('/').filter(x => x);
-
     const handleOpenSettings = () => setOpenSettings(!openSettings)
 
     return (
-        <>
-            <div className='menu-principal-container'>
-                <MenuIcon className='menu-lateral-icon' onClick={handleOpenSideMenu} />
-                {(pathnames.length == 5) ?
-                    (<Breadcrumbs separator="›" aria-label="breadcrumb">
-
-                        <Link color="inherit" to={`/my-projects/${pathnames[1]}`}>
-                            Proyecto {`${pathnames[1]}`}
+        <Box sx={{ flexGrow: 1, margin: '30px' }}>
+            <AppBar position="absolute">
+                <Toolbar>
+                    <Box sx={{ flexGrow: 1 }}>
+                        <MenuIcon onClick={handleOpenSideMenu} />
+                    </Box>
+                    <Box sx={{ flexGrow: 8, textAlign: 'center' }}>
+                        {(pathnames.length == 5) ?
+                            (<Breadcrumbs separator="›" aria-label="breadcrumb">
+                                <Link color="inherit" to={`/my-projects/${pathnames[1]}`}>
+                                    <Typography variant="body1">Proyecto {`${pathnames[1]}`}</Typography>
+                                </Link>
+                                <Link color="inherit" to={`/my-projects/${pathnames[4]}/epics/${pathnames[3]}`}>
+                                    <Typography variant="body1">Epica {`${pathnames[3]}`}</Typography>
+                                </Link>
+                                <Typography color="textPrimary" variant="body1">Historia {`${pathnames[4]}`}</Typography>
+                            </Breadcrumbs>
+                            ) : (pathnames.length == 4) ? (
+                                <Breadcrumbs separator="›" aria-label="breadcrumb">
+                                    <Link color="inherit" to={`/my-projects/${pathnames[1]}`}>
+                                        <Typography variant="body1">Proyecto {`${pathnames[1]}`}</Typography>
+                                    </Link>
+                                    {pathnames.length > 3 && <Typography color="textPrimary" variant="body1">Epica {`${pathnames[3]}`}</Typography>}
+                                </Breadcrumbs>
+                            ) : (pathnames.length == 2) ? (
+                                <Breadcrumbs separator="›" aria-label="breadcrumb">
+                                    <Typography color="textPrimary" variant="body1">Proyecto {`${pathnames[1]}`}</Typography>
+                                </Breadcrumbs>
+                            )
+                                : <Typography variant="h6">MENU</Typography>}
+                    </Box>
+                    <Box sx={{ flexGrow: 1, textAlign: 'right' }}>
+                        <Link to={'/settings'}>
+                            <SettingsIcon onClick={handleOpenSettings} />
                         </Link>
-                        <Link color="inherit" to={`/my-projects/${pathnames[4]}/epics/${pathnames[3]}`}>
-                            Epica {`${pathnames[3]}`}
-                        </Link>
-                        <Typography color="textPrimary">Historia {`${pathnames[4]}`}</Typography>
-                    </Breadcrumbs>
-                    ) : (pathnames.length == 4) ? (
-                        <Breadcrumbs separator="›" aria-label="breadcrumb">
-                            <Link color="inherit" to={`/my-projects/${pathnames[1]}`}>
-                                Proyecto {`${pathnames[1]}`}
-                            </Link>
-                            {pathnames.length > 3 && <Typography color="textPrimary">Epica {`${pathnames[3]}`}</Typography>}
-                        </Breadcrumbs>
-                    ) : (pathnames.length == 2) ? (
-                        <Breadcrumbs separator="›" aria-label="breadcrumb">
-                            <Typography color="textPrimary">Proyecto {`${pathnames[3]}`}</Typography>
-                        </Breadcrumbs>
-                    )
-                        : "MENU"}
-                <Link className='menu-principal-link' to={'/settings'}>
-                    <SettingsIcon className='settings-icon' onClick={handleOpenSettings} />
-                </Link>
-            </div>
-        </>
+                    </Box>
+                </Toolbar>
+            </AppBar>
+        </Box>
     );
 };
 
